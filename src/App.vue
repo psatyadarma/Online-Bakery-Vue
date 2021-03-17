3b7cfd4f00ef2dfcf6ecfde06b69dc58fb1fb75a
Prawira Satya Darma <e0407665@u.nus.edu>
2021-03-17 11:19:50 +0800

    first

---
+++ src/App.vue
@@ -0,0 +1,51 @@
+<template>
+  <div id="app">
+    <app-header></app-header>
+    
+    
+    <router-view></router-view>
+   
+    <!--<custBtn></custBtn>-->
+    
+
+    <app-footer></app-footer>
+   </div>
+</template>
+
+<script>
+//Register Locally
+
+import Header from './components/Header.vue'
+import Footer from './components/Footer.vue'
+
+export default {
+ data(){
+    return{
+      title:'Your first Vue Component',
+      
+    }
+  },
+   methods:{
+     greeting:function(){
+       return 'Week 6 of BT3103';
+     }
+   },
+   //Register Locally
+  components:{
+    'app-header':Header,
+    'app-footer':Footer
+    
+  }
+
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;  
+  text-align: center;
+  color: #4c2792be;
+  font-size:14px;
+}
+
+</style>
